8a46be78e352f3507785f4a27e7fae34e9db3965
Daniel Boj <dboj@uoc.edu>
2025-11-01 21:56:11 +0100

    Update Commit -> Corregido docker-compose, nuevo script para configurar automáticamente el entorno, actualizado README
    Signed-off-by: Daniel Boj <dboj@uoc.edu>

--- README.md
+++ README.md
@@ -5,9 +5,14 @@ Running WordPress with Docker
 - Docker installed on your machine. You can download it from [here](https://www.docker.com/get-started).
 - Basic knowledge of Docker and command line.
 - A text editor to create and edit files.
+- No service listening on ports 80 or 443. If not, reasign the exposed 'ports' for 'traefik' service on docker-compose.yml file, please.
 ## Steps
-- Copy .env.example files and rename whithou the .example extension
-- Fulfill the variables in the .env files
+- Check & fulfill the variables in the example 'env' files:
+  env-example
+  marisadb-env-example
+  traefik-env-example
+  wordpress-env-example
+- Execute `bash init.sh`
 - Run the following command to start the containers:
 ```bash
 docker compose up -d
@@ -26,6 +31,23 @@ docker compose down
 docker compose pull
 docker compose up -d
 ```
+## Traefik Certs on Private Tunnels as Cloudflared
+1. Go to 'docker-compose.yml'
+2. Replace this line '- "--certificatesresolvers.letsencrypt.acme.tlschallenge=true"
+' ...
+3. For those ones:
+- "--certificatesresolvers.letsencrypt.acme.dnschallenge=true"
+- "--certificatesresolvers.letsencrypt.acme.dnschallenge.provider=cloudflare"
+- "--certificatesresolvers.letsencrypt.acme.dnschallenge.delayBeforeCheck=0"
+- "--certificatesresolvers.letsencrypt.acme.dnschallenge.delayBeforeCheck=30"
+4. Add this envs in .traefik.env:
+  CLOUDFLARE_EMAIL=your_cloudflare_email
+  -> If you only have a global API KEY add this env:
+  CLOUDFLARE_API_KEY=your_api_key
+  -> If you could set or you have Token Keys for DNS Zone Edit, try one of those:
+  CLOUDFLARE_DNS_TOKEN_KEY=your_api_key_for_dns_zone_edit
+  CLOUDFLARE_ZONE_TOKEN_KEY=your_api_key_for_dns_zone_edit
+
 ## Additional Notes
 - You can customize the `docker-compose.yml` file to change ports, volumes, and other settings as needed.
 - Make sure to back up your WordPress data regularly, especially if you're using this setup for production.
---
+++ init.sh
@@ -0,0 +1,88 @@
+!#/bin/bash
+
+set -euo pipefail
+
+# Verify that the files are available
+for file in env-example mariadb-env-example traefik-env-example wordpress-env-example; do
+		if [[ ! -f "$file" ]]; then
+				echo "File '$file' doesn't exists! \n Please download from Git again."
+				exit 1;
+		fi
+done
+
+# Verify that htpasswd command is installed
+if ! command -v htpasswd &>/dev/nul; then\
+		echo 'htpaswd command is not installed, you have to install apache2-utils package, please'
+		exit 1
+fi
+
+# Rename example env files
+mv env-example .env
+mv mariadb-env-example .mariadb.env
+mv traefik-env-example .traefik.env
+mv wordpress-env-example .wordpress.env
+
+# Asking user for variables
+prompt_var() {
+		local var_name="$1"
+		local default_value="${2:-}"
+		local secret="${3:-false}"
+
+		if [ "$secret" = "true" ]; then
+				read -s -p "Set your password for $var_name: " value
+				echo
+		else
+				read -p "Set the value for $var_name: " value
+		fi
+
+		echo '${var_name} is ${value}'
+}
+
+# MariaDB env
+echo 'Setting MariaDB...'
+{
+  prompt_var "MARIADB_DATABASE"
+  prompt_var "MARIADB_USER"
+  prompt_var "MARIADB_PASSWORD" '' true
+  prompt_var "MARIADB_ROOT_PASSWORD" '' true
+  echo "ALLOW_EMPTY_PASSWORD=no"
+} > .mariadb.env
+
+# Traefik
+echo 'Setting Traefik'
+{
+  prompt_var "TRAEFIK_ACME_EMAIL"
+  prompt_var "TRAEFIK_HOSTNAME"
+  echo "Setting the password for TRAEFIK_BASIC_AUTH (user: traefikadmin)"
+  read -s -p "Please, set a secure password for traefikadmin: " traefik_pass
+  echo
+  basic_auth=$(htpasswd -n traefikadmin "$traefik_pass" | sed 's/\\$/\\\\$/g')
+  echo "TRAEFIK_BASIC_AUTH='${basic_auth}'"
+} > .traefik.env
+
+# Wordpress
+echo 'Setting WordPress...'
+{
+  prompt_var "WORDPRESS_TABLE_PREFIX"
+  prompt_var "WORDPRESS_DATABASE_USER"
+  prompt_var "WORDPRESS_DATABASE_PASSWORD" '' true
+  prompt_var "WORDPRESS_DATABASE_NAME"
+  prompt_var "WORDPRESS_HOSTNAME"
+} > .wordpress.env
+
+# .env
+echo 'Setting .env'
+{
+		prompt_var "PROJECT_DOMAIN"
+} > .env
+
+# Generate the final .env file
+cat .wordpress.env >> .env
+cat .traefik.env >> .env
+cat .mariadb.env >> .env
+
+echo 'Environment ready! Please, check the values:'
+echo "  - .env"
+echo "  - .traefik.env"
+echo "  - .mariadb.env"
+echo "  - .wordpress.env"
